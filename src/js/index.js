d7a111d7bc5aa4a69861b63be6c549e63285282b
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-01-19 16:40:44 +0800

    🌺Finale🌺

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 const hamburger = document.getElementById('nav-icon4')
 const navMobile = document.getElementById('nav-mobile')
+const nav = document.getElementById('nav')
 
 // hamburger.classList.add('open')
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('open')
   navMobile.classList.toggle('open')
+  nav.classList.toggle('fixed')
 })
